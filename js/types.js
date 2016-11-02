01c708f7c2612ffd78db347c69187caeabe3da24
Tobias Thurnreiter <thurnreiter@gmail.com>
2016-11-02 17:58:54 +0100

    step 4

---
+++ js/types.js
@@ -0,0 +1,12 @@
+
+// We use single quotes in creating strings.
+
+const a = 'I am a string!';
+console.log(a);
+
+const person1 = {};
+
+person1.firstName = 'John';
+person1.lastName = 'Doe';
+
+console.log(person1.firstName + ' ' + person1.lastName);
